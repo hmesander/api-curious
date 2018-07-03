8086fc200c72521377aceb1f89c510b2b3d81e48
hmesander <haleymesander@gmail.com>
2018-07-02 21:56:45 -0600

    WIP oauth setup and testing with omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+
+  get '/login/oauth/authorize', as: :github_login, to: 'sessions#create'
+  get '/dashboard', to: 'dashboard#index'
 end
