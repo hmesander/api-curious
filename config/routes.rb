05ae8640affc534fb23d46ab986bedc3fc80b908
hmesander <haleymesander@gmail.com>
2018-07-04 14:05:38 -0600

    WIP Adds ability of user to see their profile information --needs work on testing

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   get '/dashboard', to: 'dashboard#index'
   get '/logout', to: 'sessions#destroy'
   get '/repos', to: 'repos#index'
+
+  resources :users, only: [:show]
 end
