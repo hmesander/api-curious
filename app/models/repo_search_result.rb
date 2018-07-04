0b3d68fa7ce70d92d203eec5c7ae278051a0121a
hmesander <haleymesander@gmail.com>
2018-07-04 11:50:22 -0600

    Updates api calls to be dynamic based on token of current user

--- app/models/repo_search_result.rb
+++ app/models/repo_search_result.rb
@@ -1,14 +1,15 @@
 class RepoSearchResult
-  def initialize(user_token)
-    @user_token = user_token
+  def initialize(current_user)
+    @user_token = current_user.token
+    @nickname = current_user.nickname
   end
 
   def repos
-    conn = Faraday.new(:url => 'https://api.github.com') do |faraday|
-      faraday.headers['access_token'] = ENV['token']
-      faraday.adapter Faraday.default_adapter
-    end
-    response = conn.get('/users/hmesander/repos')
+    conn = Faraday.new(url: 'https://api.github.com',
+                       headers: {
+                         'access_token' => @user_token
+                       })
+    response = conn.get("/users/#{@nickname}/repos?per_page=100")
     data = JSON.parse(response.body, symbolize_names: true)
     data.map do |raw_repo|
       Repo.new(raw_repo)
